dfb0e9a1dc57078fb357b350fa729944fd58b063
mathias <mathias15052004@gmail.com>
2023-04-16 11:11:49 -0300

    Updated index.js file from migrations folder

---
+++ src/database/sqlite/migrations/index.js
@@ -0,0 +1,12 @@
+const sqliteConnection = require('../../sqlite')
+const createUser = require('./createUsers')
+
+async function migrationsRun() {
+    const schemas = [
+        createUser
+    ].join('')
+
+    sqliteConnection().then(db => db.exec(schemas)).catch(error => console.log(error))
+}
+
+module.exports = migrationsRun
