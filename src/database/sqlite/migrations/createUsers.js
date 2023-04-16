7250326a537d4e97d8a046b6656ffa721c675c6b
mathias <mathias15052004@gmail.com>
2023-04-16 11:05:31 -0300

    Updated createUsers.js file from migrations folder

---
+++ src/database/sqlite/migrations/createUsers.js
@@ -0,0 +1,11 @@
+const createUser = `
+    id integer primary key autoincrement,
+    name varchar,
+    email varchar,
+    password varchar,
+    avatar varchar null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
+`
+
+module.exports = createUser
