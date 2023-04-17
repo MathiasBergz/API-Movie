6d1ed99c25b819793d174237666a4e9a2e2d7e7c
mathias <mathias15052004@gmail.com>
2023-04-17 14:48:38 -0300

    Updated serve.js file

--- src/server.js
+++ src/server.js
@@ -1,6 +1,32 @@
-const express = require('express')
+require('express-async-errors')
 
+const migrationsRun = require('./database/sqlite/migrations')
+const AppError = require('./utils/AppError')
+const express = require('express')
 const app = express()
+app.use(express.json())
+
+const routes = require('./routes')
+
+app.use(routes)
+
+migrationsRun()
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    console.log(error)
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error'
+    })
+})
 
 const PORT = 3333
 
